2bf7ff6a09caafcba2762fb61462106faf43cca5
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-12-11 15:54:51 +0300

    mola

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, NgModule, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { FormsModule } from '@angular/forms'; 
+import { NgModel } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [FormsModule],
   template:`<h1>Angular 19 Yenilikleri</h1>
-  <input type="text" value="ali can yücel">
+  <input [(ngModel)]="name"/>
   `,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Angular 19';
+  name=signal<string>("");
 }
--- src/yenillikler.md
+++ src/yenillikler.md
@@ -1,1 +1,5 @@
 HMR sayesinde style uyguladıgımız zaman sayfa yenilenmeden style cıktısını gorebiliyoruz
+Standalone yapısı default olarak true olarak geliyor zorunlu kılınmıs default değerinde
+csp ile uygulama build edildiğinde html sayfasında style ve sciprtlerin linklerin kriptolonmasını başka birinin farklı bir style uygulamayammasını sağlıyor xxs saldırılarından koruyor
+
+
