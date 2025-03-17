28fb226d5da9edae52ed632014a89eca7e50862a
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2025-03-17 23:54:28 +0300

    mola.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,42 @@
-import { Component, NgModule, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, computed, ElementRef, input, Input, linkedSignal, NgModule, Output, output, signal, viewChild, ViewChild, ViewChildren } from '@angular/core';
 import { FormsModule } from '@angular/forms'; 
-import { NgModel } from '@angular/forms';
-import { CommonModule } from '@angular/common';
 @Component({
+  //stanalone default olarak true şeklinde geliyor app module kullanamk istiyorsak standalone false yazmamız lazım//
   selector: 'app-root',
   imports: [FormsModule],
   template:`<h1>Angular 19 Yenilikleri</h1>
-  <input [(ngModel)]="name"/>
+  <!-- hot reload css içerisinde değişilşik yaptıgımızda  -->
+  <input [(ngModel)]="name">
+  <p>{{value}}</p>
+  
   `,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Angular 19';
   name=signal<string>("");
+  fullName=computed(()=>{
+    // işlemler
+    return "bay" + this.name();
+  });
+  // fullNameLinked=linkedSignal(()=>{
+  //   //işlemler
+  //   return "bay" + this.name;
+  // })
+
+  fullNameLinked=linkedSignal({
+    source:this.name,
+    computation:((newversion,prevversion)=>{
+    console.log("new version:" + newversion);
+    console.log("old version:" + prevversion?.value);
+    return "..." + this.name();
+    })
+  });
+  readonly value= input.required<string>;
+  readonly event=output<string>();
+  readonly h1e1=viewChild.required<ElementRef<HTMLHeadingElement>>("h1element");
+  constructor(){
+     
+   // this.value.set("test"); // set edilemez
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, input, resource } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   imports: [],
-  template:``,
+  template: ``,
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  userId = input.required<number>()
+  #http = inject(HttpClient);
+  data = resource({
+    request: this.userId,
+    loader: async ({ request: id }) => {
+      const result = await lastValueFrom(this.#http.get('https://jsonplaceholder.typicode.com/users/${id}'));
+      return result;
+    }
+  })
 }
